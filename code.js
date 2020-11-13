f1af46b7fbce758087c143f49322a0881d12c084
phoebe njoroge <phoebe.njoroge@gmail.com>
2020-11-13 09:51:38 -0600

    changes to test.js

--- code.js
+++ code.js
@@ -17,3 +17,23 @@ function isFive(input){
     }else
         return false;
 }
+function isEven(input){
+    if(input % 2 === 0){
+        return true;
+    }else
+        return false;
+}
+function isVowel(input){
+    if(input === 'a' || input === 'e' || input === 'i'|| input === 'o'|| input === 'u'){
+        return true;
+    }else if (input === 'A' || input === 'E' || input === 'I'|| input === 'O'|| input === 'U'){
+        return true;
+    }else
+        return false;
+}
+function add(num1, num2){
+    if(isNaN(num1)|| isNaN(num1)){
+        return NaN;
+    }else
+    return parseFloat(num1)+ parseFloat(num2);
+}
--- tests.js
+++ tests.js
@@ -43,10 +43,67 @@ describe("isFive", function (){
     it('should return a boolean value when function is run', function () {
         expect(typeof isFive ()).toBe('boolean');
     });
-    it('should return a true if is parsed through', function () {
+    it('should return true if is parsed through', function () {
         expect(isFive(5)).toBe(true);
     });
-    it('should return a false if 4 is parsed through', function () {
+    it('should return false if 4 is parsed through', function () {
         expect(isFive(4)).toBe(false);
     });
+})
+describe("isEven", function (){
+    it('should be a defined function', function () {
+        expect(typeof isEven).toBe('function');
+    });
+    it('should return a boolean value when function is run', function () {
+        expect(typeof isEven ()).toBe('boolean');
+    });
+    it('should return false if 3 is parsed through', function () {
+        expect(isEven(3)).toBe(false);
+    });
+    it('should return true if 4 is parsed through', function () {
+        expect(isEven(4)).toBe(true);
+    });
+    it('should return false if Infinity is parsed through', function () {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it('should return false if "Banana" is parsed through', function () {
+        expect(isEven("Banana")).toBe(false);
+    });
+})
+describe("isVowel", function (){
+    it('should be a defined function', function () {
+        expect(typeof isVowel).toBe('function');
+    });
+    it('should return a boolean value when function is run', function () {
+        expect(typeof isVowel()).toBe('boolean');
+    });
+    it('should return false if "b" is parsed through', function () {
+        expect(isVowel("b")).toBe(false);
+    });
+    it('should return true if "a" is parsed through', function () {
+        expect(isVowel("a")).toBe(true);
+    });
+    it('should return true if "A" is parsed through', function () {
+        expect(isVowel("A")).toBe(true);
+    });
+    it('should return false if "4" is parsed through', function () {
+        expect(isVowel("4")).toBe(false);
+    });
+})
+describe("add", function (){
+    it('should be a defined function', function () {
+        expect(typeof add).toBe('function');
+    });
+    it('should return a number value when function is run', function () {
+        expect(typeof add()).toBe('number');
+    })
+    it('should return 5 when 2, 3 are parsed', function () {
+        expect(add(2,3)).toBe(5);
+    })
+    it('should return 11 when "5", 6 are parsed', function () {
+        expect(add("5",6)).toBe(11);
+    })
+    it('should return NaN when "banana" and "split" are parsed', function () {
+        expect(add("banana","split")).toBe(NaN);
+    })
 })
